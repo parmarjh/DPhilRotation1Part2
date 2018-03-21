381f895e79cbbfec881e3ad70c7e4224159455d7
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-03-21 20:20:27 +0000

    Have to reprocess in order to get the pickles

--- DataAnnotationExtraction/DataExtractionForMutagenesis.py
+++ DataAnnotationExtraction/DataExtractionForMutagenesis.py
@@ -29,7 +29,8 @@ def OptionParsing():
                       help="Used in conjunction with --cancer_dir to only process one cancer type directory.")
     parser.add_option('-c', '--cancer_name', dest='cancerName', default=None,
                       help="Cancer directory name to be processed. List of names can be found in CancerTypes.txt")
-    parser.add_option('-f', '--build_final', dest='buildFinal', default=None, help="Instructions to build the final matrix for extracting sequences for CNN predictions.")
+    parser.add_option('-f', '--build_final', dest='buildFinal', default=False, action='store_true', help="Instructions to build the final matrix for extracting sequences for CNN predictions.")
+    parser.add_option('-u', '--unit_test', dest='unitTest', default=False, action='store_true', help='Use with --build_final for development on only a subset of the cancer groups.')
     parser.add_option('-s', '--no_stats', dest="noStats", default=True, action="store_false", help="Flag to disable statistics")
     parser.add_option('-z', '--clean', dest="clean", default=False, action="store_true")
     (options, args) = parser.parse_args()
@@ -93,7 +94,7 @@ class CancerData:
     def __init__(self, FilePath, Options, CancerType):
         self.FilePath = FilePath
         self.cancer = CancerType
-        self.vcfData = PatientData(Options, "%sPCAWGData/Cancers/%s/annotated/" % (FilePath.rstrip("DataAnnotationExtraction"), Options.cancerName))
+        self.vcfData = PatientData(Options, "%sPCAWGData/Cancers/%s/annotated/" % (FilePath.rstrip("DataAnnotationExtraction"), self.cancer))
 
 class PatientData:
     def __init__(self, Options, vcfPath):
@@ -156,6 +157,7 @@ class PatientData:
 
             genomicPosOnly = ['\t'.join(pos.split('\t',3)[0:3]) for pos in allPos]
             print("INFO: Total shared genomic sites (ignoring specific mutations): %s"%(len(allPos)-len(list(set(genomicPosOnly)))))
+            genomicPosOnly = list(set(genomicPosOnly))
 
             allBedsOut = '/'.join(self.bedFiles[0].split('/')[0:len(self.bedFiles[0].split('/'))-1]) + '/allSites.bed'
             if os.path.isfile(allBedsOut)==False:
@@ -247,7 +249,6 @@ class PatientData:
                     except KeyError:
                         info = None
 
-
                     if info is not None:
                         info=info.split('\t')
                         if len(info[3])==1 and len(info[4])==1: # Capture SNP
@@ -316,8 +317,140 @@ class PatientData:
         else:
             return(vType, None)
 
+class MatrixMerger:
+    def __init__(self, FilePath, CancersToProcess, Options):
+        self.FilePath = FilePath
+        self.cancers = CancersToProcess
+        self.thePickles = self.LoadThePickles(Options)
+        self.valueMatrix, self.priorityMatrix, self.posMatrix, self.headerMatrix, self.cancerMatrix = self.MergeThePicklesInards()
+
+    def LoadThePickles(self, Options):
+        # Pickle Rick!!!!
+        print("INFO: Loading the pickles.")
+        funcPickles = {}
+        for i, c in enumerate(self.cancers):
+            UpdateProgress(i, len(self.cancers), "Loading pickles...%s"%(c))
+            zippedPickle = self.FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p.gz'%(c))
+            readyPickle = self.FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p'%(c))
+
+            if os.path.isfile(zippedPickle)==True:
+                os.system('gzip -d %s'%(zippedPickle))
+                try:
+                    # Processes files that are too large to load through pickle.
+                    with open(readyPickle, 'rb') as incomingPickle:
+                        indPickle = pickle.load(incomingPickle)
+                except Exception as e:
+                    if os.path.isfile(zippedPickle)==False and os.path.isfile(readyPickle)==True:
+                        os.system('gzip %s'%(readyPickle))
+                    print('')
+                    print("Warning: Unable to process...%s" % (c))
+                    print("INFO: Reprocessing CancerData class.")
+                    indPickle = CancerData(self.FilePath, Options, c)
+                if os.path.isfile(readyPickle)==True:
+                    os.system('gzip %s' % (readyPickle))
+            else:
+                with open(readyPickle, 'rb') as incomingPickle:
+                    indPickle = pickle.load(incomingPickle)
+                if os.path.isfile(readyPickle)==True:
+                    os.system('gzip %s' % (readyPickle))
+
+            try:
+                funcPickles.update({c:indPickle[c]})
+            except KeyError:
+                sys.exit("ERROR: Cancer name does not match the pickle.")
+
+        print('')
+
+        return(funcPickles)
+
+    @fn_timer
+    def MergeThePicklesInards(self):
+        '''
+        Extracts the information for each of the cancer types and create a group of matrices that are merged for all cancer types.
+        Within the matrix the following IDs hold:
+            SNP-NonCoding = 1.
+            INS-NonCoding = 2.
+            DEL-NonCoding = 3.
+            CodingOnly = 0.
+
+        :return: Row ID matrix, Header ID matrix, Priority Site Matrix, Binary Matrix, Cancer Matrix to know which column is which cancer.
+        '''
+        print("INFO: Merging mutation information.")
+
+        # Extract the headers and build array for cancer type.
+        cancerHeaders, cancerMatrix = self.GetHeaders()
+
+        # Get all positions across all cancers, if duplicated, make note.
+        nrow, allPositions, uniquePos = self.GenomicPositionMerger()
+
+        # Step 0 Get the matrices into a list
+        matrixHolder = [self.thePickles[cancer].vcfData.binaryMatrix for cancer in self.cancers]
+        # Step 1 set up a new array for the merged valueMatrix
+        valueMatrix = np.zeros(shape=(nrow, cancerHeaders.shape[0]))
+
+        # Step 2 Fill in the valueMatrix
+        for i in range(0,uniquePos.shape[0]): # Loops over Genomic Positions (rows)
+
+            jadjust = 0 # Do This at the end to adjust the column by number of samples (headers).
+
+            pos = uniquePos[i,]
+
+            for l in range(0,len(matrixHolder)): # Loops through cancer values
+
+                for j in range(0,matrixHolder[l].shape[1]): # Loops over columns
+
+                    # Substep 1: check if this cancer has that position in it.
+                    ogRow = np.asarray(np.where(allPositions[l]==pos)[0])
+
+                    if ogRow.shape[0]!=0:
+
+                        if ogRow.shape[0] > 1:
+                            print("INFO: Inquiring position...")
+                            print(pos)
+                            print("INFO: Position Indices...")
+                            print(ogRow)
+                            print("INFO: Reported Positions...")
+                            for shit in range(0,ogRow.shape[0]):
+                                print([allPositions[l][ogRow[shit]]])
+                            time.sleep(1)
+                            sys.exit("ERROR: Duplicate positions found in matrix. See the line above this.")
+
+                        else:
+                            # print("INFO: Inquiring position...")
+                            # print(pos)
+                            # print("INFO: Reported Positions...")
+                            # print(allPositions[l][ogRow[0],])
+
+                            valueMatrix[i,jadjust] = matrixHolder[l][ogRow[0],j] # Set value inside of the new matrix to proper value
+
+                    jadjust += 1 # Do This at the end to adjust the column by number of samples (headers).
+
+        return(valueMatrix, None, uniquePos, cancerHeaders, cancerMatrix)
+
+    def GetHeaders(self):
+        cancerMatrix = []
+        cancerHeaders = []
+        for cancer in self.cancers:
+            cancerHeaders.append(self.thePickles[cancer].vcfData.headerMatrix)
+            cancerMatrix.append(np.asarray([cancer for i in range(0, self.thePickles[cancer].vcfData.headerMatrix.shape[0])]))
+        return(np.concatenate(cancerHeaders),np.concatenate(cancerMatrix))
+
+    def GenomicPositionMerger(self):
+        '''
+        The length of the unique Positions is the number of rows in the final valueMatrix. Return that and the new positions.
+
+        :return: Number of rows needed in valueMatrix, a list of all positions for each cancer, the final Position matrix
+        '''
+        allPositions = [self.thePickles[cancer].vcfData.posMatrix for cancer in self.cancers]
+        mergedPos = np.concatenate([self.thePickles[cancer].vcfData.posMatrix for cancer in self.cancers])
+        uniquePos = np.unique(mergedPos)
+        nrows = uniquePos.shape[0]
+
+        return(nrows, allPositions, uniquePos)
+
+
 def GatherData(FilePath, Options):
-    print("INFO: Begging the process...")
+    print("INFO: Beginning the process...")
     with open(FilePath.rstrip("DataAnnotationExtraction") + "PCAWGData/CancerTypes.txt", 'r') as inFile:
         cancerTypes = [line.rstrip('\n') for line in inFile.readlines()]
 
@@ -325,10 +458,9 @@ def GatherData(FilePath, Options):
     if Options.oneCancer:
         if Options.cancerName not in cancerTypes:
             sys.exit("ERROR: Unrecognized cancer_name argument provided.")
-        pickleFile = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p') % (Options.cancerName)
-        if os.path.isfile(pickleFile):
-            with open(pickleFile,'rb') as f:
-                allData = pickle.load(f)
+        pickleFile = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p.gz') % (Options.cancerName)
+        readyPickle = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p') % (Options.cancerName)
+        if os.path.isfile(pickleFile) or os.path.isfile(readyPickle):
             print("INFO: Pickled class exists. Nothing to do.")
         else:
             print("INFO: Processing %s" % (Options.cancerName))
@@ -337,11 +469,38 @@ def GatherData(FilePath, Options):
                 pickle.dump(allData, f)
     else:
         for cancer in cancerTypes:
-            pickleFile = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p')%(cancer)
-            if os.path.isfile(pickleFile):
-                pass
-            print("INFO: Processing %s" % (cancer))
-            allData.update({cancer: CancerData(FilePath, Options, cancer)})
+            pickleFile = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p.gz')%(cancer)
+            readyPickle = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p') % (Options.cancerName)
+            if os.path.isfile(pickleFile) or os.path.isfile(readyPickle):
+                print("INFO: Pickled class exists. Nothing to do.")
+            else:
+                print("INFO: Processing %s" % (cancer))
+                allData.update({cancer: CancerData(FilePath, Options, cancer)})
+
+def MergeMatrices(FilePath, Options):
+    print("INFO: Begging the process...")
+    with open(FilePath.rstrip("DataAnnotationExtraction") + "PCAWGData/CancerTypes.txt", 'r') as inFile:
+        cancerTypes = [line.rstrip('\n') for line in inFile.readlines()]
+
+    toProcess = []
+    notFound = []
+    for cancer in cancerTypes:
+        thePath = FilePath.replace('DataAnnotationExtraction', 'PCAWGData/Cancers/%s/CancerDataClass.p.gz'%(cancer))
+        if os.path.isfile(thePath)==True:
+            toProcess.append(cancer)
+        else:
+            notFound.append(cancer)
+
+    if notFound != []:
+        print("WARNING: The following cancers were not found...")
+        print(' '.join(notFound))
+
+    if Options.unitTest:
+        toProcess=['BLCA','BOCA']
+
+    allData = MatrixMerger(FilePath, toProcess, Options)
+
+
 
 @fn_timer
 def main():
@@ -353,7 +512,10 @@ def main():
     Config.read(localpath + "usr_paths.ini")
     snpEFF = ConfigSectionMap(Config.sections()[0], Config)  # get snpEFF path
 
-    GatherData(FilePath, Options)
+    if Options.buildFinal == False:
+        GatherData(FilePath, Options)
+    else:
+        MergeMatrices(FilePath, Options)
 
 
 if __name__=='__main__':
