d05a8a37bafdefa01d5b85a52abd9f049c40c6de
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-05-16 23:14:38 +0100

    Ready for predictions I think

--- MutagenesisExtractionAndProcess/GrandPredictor.py
+++ MutagenesisExtractionAndProcess/GrandPredictor.py
@@ -17,6 +17,7 @@ try:
     import sys
     import os
     import glob
+    from collections import OrderedDict
 
     import keras as ks
     import numpy as np
@@ -35,7 +36,8 @@ def OptionParsing():
     usage = 'usage: %prog [options]'
     parser = OptionParser(usage)
     parser.add_option('-m', '--model', dest="InputDir", default="/Users/schencro/Desktop/Oxford/Rotation_1/CNN/Model/AllENCODEnocancer.23Feb2018.batch64.2018-02-23.21.59/", help="Directory where the model is located to make the predictions.")
-    parser.add_option('-o', '--output', dest="output", default="Predictions/", help="Output directory")
+    parser.add_option('-o', '--output', dest="output", default="/Users/schencro/Desktop/Oxford/Rotation_1/PCAWGArm/MutagenesisExtractionAndProcess/Predictions/WTPreds.txt", help="Output directory")
+    parser.add_option('-i', '--input', dest='infasta', default='/Users/schencro/Desktop/Oxford/Rotation_1/PCAWGArm/MutagenesisExtractionAndProcess/WTseqs.fasta', help="Input fasta file")
     parser.add_option('-d', '--debug', dest='debug', default=False, action='store_true', help="Run to do small batch predictions.")
     (options, args) = parser.parse_args()
     return (options, parser)
@@ -75,17 +77,113 @@ def LoadModel(Options):
 
     return(loaded_model)
 
+def dna_one_hot(seq, seq_len=None, flatten=True):
+    '''
+    :param seq: Input sequence read from fasta file.
+    :param seq_len: Length of longest sequence
+    :param flatten: Whether or not to flatten into a column vector (May Change)
+    :return: seq_vec: A Flattened column vector (May change)
+    '''
+    if seq_len == None:
+        seq_len = len(seq)
+        seq_start = 0
+    else:
+        if seq_len <= len(seq):
+            # trim the sequence
+            seq_trim = (len(seq)-seq_len) // 2
+            seq = seq[seq_trim:seq_trim+seq_len]
+            seq_start = 0
+        else:
+            seq_start = (seq_len-len(seq)) // 2
+
+    seq = seq.upper()
+
+    seq = seq.replace('A','0')
+    seq = seq.replace('C','1')
+    seq = seq.replace('G','2')
+    seq = seq.replace('T','3')
+
+    # map nt's to a matrix 4 x len(seq) of 0's and 1's.
+    #  dtype='int8' fails for N's
+    seq_code = np.zeros((seq_len,4), dtype='float16')
+    for i in range(seq_len):
+        if i < seq_start:
+            seq_code[i:] = 0.25
+        else:
+            try:
+                seq_code[i,int(seq[i-seq_start])] = 1
+            except:
+                seq_code[i:] = 0.25
+
+    # flatten and make a column vector 1 x len(seq)
+    seq_vec = seq_code.flatten()[None,:]
+    # seq_vec = seq_code
+    # print(seq_vec.reshape(600,4)) # This is the appropriate shape after flattening...
+
+    return seq_vec
+
+def RunPredictions(seq_vecs, model, Options):
+    # Step 1 separate headers and chunks
+    seqs = []
+    heads = []
+    for head in seq_vecs:
+        seqs.append(seq_vecs[head])
+        heads.append(head)
+
+    seqStack = np.vstack(seqs)
+    seqsReady = seqStack.reshape(seqStack.shape[0], 600, 4)
+
+    preds = model.predict(seqsReady)
+    #
+    # # outLine = (heads[0] + '\t' + '\t'.join([repr(i) for i in preds.tolist()[0]]) +'\n')
+    outLine = heads[0] + '\t' + repr(np.mean(preds)) + '\t' + repr(np.std(preds)) + '\n'
+    with open(Options.output, 'a') as outFile:
+        outFile.write(outLine)
+
+
+@fn_timer
+def ProcessFasta(Options, model):
+    #### Takes two and a half hours to process one full PCAWG fasta set (2.25 * 2 for both WT and Mut) with chunk size of 250000
+    i = 0
+    seq = ''
+    chunkSize = 1
+    seq_vecs = OrderedDict()
+    with open(Options.infasta, 'r') as inFile:
+        for line in inFile:
+            if line[0] == '>':
+                if seq:
+                    i += 1
+                    # Main code chunck
+                    ###
+                    if len(seq_vecs)==chunkSize-1:
+                        seq_vecs[header] = dna_one_hot(seq, 600) # Not flattened
+                        # do something
+                        RunPredictions(seq_vecs, model, Options)
+                        # if i==1:
+                        #     break
+                        # TODO Make predictions once there are 250000 seqs put together
+                        ##
+                        seq_vecs=OrderedDict()
+                    else:
+                        seq_vecs[header] = dna_one_hot(seq, 600)
+
+                    ###
+                header = line[1:].rstrip()
+                seq = ''
+            else:
+                seq += line.rstrip()
+
 @fn_timer
 def main():
     FilePath = os.path.dirname(os.path.abspath(__file__))
     localpath = os.path.abspath(__file__).rstrip('DataExtractionForMutagenesis.py')  # path to scripts working directory
 
     (Options, Parser) = OptionParsing()
-
+    os.system('touch %s' % (Options.output))
     model = LoadModel(Options)
 
     # model.summary()
-
+    ProcessFasta(Options, model)
 
 if __name__=='__main__':
     main()
